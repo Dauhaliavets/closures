3c885a579c76ed9cc23b5183ffa08be5e3f4dd83
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2021-11-18 20:41:07 +0300

    feat: task completed

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-function buildFun(n){
+function buildFun(n) {
+	var res = [];
 
-    var res = []
-
-    for (var i = 0; i< n; i++){
-        res.push(function(){
-            console.log(i)
-        })
-    }
-    return res
-
+	for (var i = 0; i < n; i++) {
+		(function (i) {
+			res.push(function () {
+				return i;
+			});
+		})(i);
+	}
+	return res;
 }
